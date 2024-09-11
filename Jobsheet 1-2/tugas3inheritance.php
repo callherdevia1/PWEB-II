4bddd9034b8f58ffaf8ec2f82a77240155865ec4
callherdevia1 <152271304+callherdevia1@users.noreply.github.com>
2024-09-12 00:44:20 +0700

    Update tugas3inheritance.php

--- Jobsheet 1-2/tugas3inheritance.php
+++ Jobsheet 1-2/tugas3inheritance.php
@@ -4,21 +4,19 @@ class Pengguna { // membuat class Pengguna
     //membuat atribut nama dan metode getNama()
     protected $nama;
 
-    public function __construct ($nama) {
-        $this->nama = $nama;
-    }
-   // Metode mengambil Nama
-    public function getNama() {
+   // Metode getNama
+    public function setNama() {
         return $this->nama;
     }
+    public function getNama() {
+        return $this->nama;          
 }
 //membuat class Dosen mewarisi dari class Pengguna
 class Dosen extends Pengguna { 
-    private $mataKuliah;//Menambahkan atribut MataKuliah
+    private $mataKuliah;
 
     // Konstruktor untuk inisialisasi nama dan mataKuliah
-    public function __construct($nama, $mataKuliah){
-        parent::__construct($nama);
+    public function setMataKuliah($matakuliah) {
         $this->mataKuliah = $mataKuliah;
     }
     // Metode untuk mengambil mata kuliah
@@ -27,9 +25,11 @@ class Dosen extends Pengguna {
     }
 }
 //Instansiasi Objek Dari Class Dosen
-$dosen1 = new Dosen("Prih Diantono Abda'u", "Pemrograman Web 2 PHP");
+$dosen1 = new Dosen();
+$dosen1->setNama("Prih Diantono Abda'u");
+$dosen1->setMataKuliah("Pemrograman Web 2");
 
 //Menampilkjan Data Dosen
-echo "Nama Dosen: " . $dosen1->getNama() . "<br>"; // Output: Nama Dosen: Prih Diantono Abda'u
-echo "Mata Kuliah: " . $dosen1->getMataKuliah() . "<br>"; // Output: Mata Kuliah: Pemrograman Web 2 PHP
+echo "Nama Dosen: " . $dosen1->getNama() . "<br>"; 
+echo "Mata Kuliah: " . $dosen1->getMataKuliah() . "<br>"; 
 ?>
