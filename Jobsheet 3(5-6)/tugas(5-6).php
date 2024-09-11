0ca58e5b656f599fb380246e89aabc7126ebc4a2
callherdevia1 <152271304+callherdevia1@users.noreply.github.com>
2024-09-12 04:17:29 +0700

    Update tugas(5-6).php

--- Jobsheet 3(5-6)/tugas(5-6).php
+++ Jobsheet 3(5-6)/tugas(5-6).php
@@ -3,10 +3,6 @@
 class Person {
     protected $name;
 
-    public function __construct($name) {
-        $this->name = $name;
-    }
-
     // Metode untuk menampilkan peran, yang akan dioverride oleh kelas turunan
     public function getRole() {
         return "Undefined Role";
@@ -15,18 +11,18 @@ class Person {
     public function getName() {
         return $this->name;
     }
+    
+    public function setName($name) {
+        $this->name = $name;
+    }
+    
 }
 
 // Kelas Dosen yang merupakan turunan dari Person
 class Dosen extends Person {
-    private $nidn; // Encapsulation
+    private $nidn; // Encapsulasi
 
-    public function __construct($name, $nidn) {
-        parent::__construct($name);
-        $this->nidn = $nidn;
-    }
-
-    // Setter dan getter untuk NIDN (Encapsulation)
+    // Setter dan getter untuk NIDN (Encapsulasi)
     public function setNidn($nidn) {
         $this->nidn = $nidn;
     }
@@ -34,8 +30,9 @@ class Dosen extends Person {
     public function getNidn() {
         return $this->nidn;
     }
+    
 
-    // Override metode getRole untuk Dosen (Polymorphism)
+    // Polymorphism: metode getRole untuk Dosen
     public function getRole() {
         return "Dosen";
     }
@@ -44,11 +41,6 @@ class Dosen extends Person {
 // Kelas Mahasiswa yang merupakan turunan dari Person
 class Mahasiswa extends Person {
     private $nim; // Encapsulation
-
-    public function __construct($name, $nim) {
-        parent::__construct($name);
-        $this->nim = $nim;
-    }
 
     // Setter dan getter untuk NIM (Encapsulation)
     public function setNim($nim) {
@@ -69,38 +61,51 @@ class Mahasiswa extends Person {
 abstract class Jurnal {
     protected $title;
 
-    public function __construct($title) {
+    public function setTitle($title) {
         $this->title = $title;
     }
+    
+    public function getTitle() {
+        return $this->title;
+    }
 
-    // Metode abstrak yang harus diimplementasikan di kelas turunan
+    // Metode abstrak di kelas turunan
     abstract public function submitJurnal();
 }
 
 // Kelas JurnalDosen sebagai turunan dari Jurnal
 class JurnalDosen extends Jurnal {
     public function submitJurnal() {
-        return "Jurnal Dosen: " . $this->title . " Pengajuan Jurnal oleh Dosen";
+        return "Jurnal Dosen: " . $this->getTitle() . " Pengajuan Jurnal oleh Dosen";
     }
 }
 
 // Kelas JurnalMahasiswa sebagai turunan dari Jurnal
 class JurnalMahasiswa extends Jurnal {
     public function submitJurnal() {
-        return "Jurnal Mahasiswa: " . $this->title . " Pengajuan Jurnal oleh Mahasiswa";
+        return "Jurnal Mahasiswa: " . $this->getTitle() . " Pengajuan Jurnal oleh Mahasiswa";
     }
 }
 
-// Instansiasi objek Dosen dan Mahasiswa
-$dosen1 = new Dosen("Prih Diantono Abda'u", "123456789");
-$mahasiswa1 = new Mahasiswa("Devia Kippuw", "230202031");
+// Instansiasi objek Dosen
+$dosen1 = new Dosen();
+$dosen1->setName("Prih Diantono Abda'u");
+$dosen1->setNidn("123456789");
+
+// Instansiasi objek Mahasiswa
+$mahasiswa1 = new Mahasiswa();
+$mahasiswa1->setName("Devia Kippuw");
+$mahasiswa1->setNim("230202031");
 
 echo $dosen1->getName() . " adalah " . $dosen1->getRole() . " dengan NIDN: " . $dosen1->getNidn() . "<br>";
 echo $mahasiswa1->getName() . " adalah " . $mahasiswa1->getRole() . " dengan NIM: " . $mahasiswa1->getNim() . "<br>";
 
-// Instansiasi objek Jurnal
-$jurnalDosen = new JurnalDosen("Pemrograman Web 1 dan 2");
-$jurnalMahasiswa = new JurnalMahasiswa("Komputer dan Bisnis");
+// Instansiasi objek Jurnal Dosen
+$jurnalDosen = new JurnalDosen();
+$jurnalDosen->setTitle("Pemrograman Web 1 dan 2");
+// Instansiasi objek Jurnal Mahasiswa
+$jurnalMahasiswa = new JurnalMahasiswa();
+$jurnalMahasiswa->setTitle("Komputer dan Bisnis");
 
 echo $jurnalDosen->submitJurnal() . "<br>";
 echo $jurnalMahasiswa->submitJurnal() . "<br>";
