907c5ac6a1b41a6e103c373a61793a2febc15cbd
callherdevia1 <152271304+callherdevia1@users.noreply.github.com>
2024-09-12 02:05:46 +0700

    Update inheritancejbs.php

--- Jobsheet 3(5-6)/inheritancejbs.php
+++ Jobsheet 3(5-6)/inheritancejbs.php
@@ -2,32 +2,28 @@
 class Person { //Membuat Class Person
     protected $name; //Atribut menyimpan Nama
 
-    public function __construct($name) {
-        $this->name = $name;
-    }
     //Metode mendapatkan nama
     public function getName() {
         return $this->name;
     }
+              
 }
-
-class Student extends Person {
+//Class student mewarisi dari person
+class Student extends Person { //atribut studentID dan getStudentID
     private $studentID;
-
-    public function __construct($name, $studentID) {
-        parent::__construct($name);
-        $this->studentID = $studentID;
-    }
 
     public function getStudentID() {
         return $this->studentID;
     }
+
 }
 
 //Instansiasi Objek Dari Class Student
-$person1 = new Student("Devia Kippuw", "230202031");
+$person1 = new Student();
+$person1->name ="Devia Kippuw";
+$person1->studentID ="230202031";
 
 //Menampilkan Data Student
-echo "Nama Person: " ."<br>" . $person1->getName() . "<br>"; // Output: Nama Dosen: Devia Kippuw
-echo "Student ID: " . $person1->getStudentID() . "<br>"; // Output: Student ID: 230202031
+echo "Nama Person: " ."<br>" . $person1->getName() . "<br>"; 
+echo "Student ID: " . $person1->getStudentID() . "<br>";
 ?>
