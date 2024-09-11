e59939f5c488f368f9099ffced3af01a32fca26c
callherdevia1 <152271304+callherdevia1@users.noreply.github.com>
2024-09-12 01:33:58 +0700

    Update atribute_metode.php

--- Jobsheet 2(3-4)/atribute_metode.php
+++ Jobsheet 2(3-4)/atribute_metode.php
@@ -1,21 +1,16 @@
 <?php
-//Definisi Class
-class Mahasiswa { //Membuat class mahasiswa
+//Class Mahasiswa
+class Mahasiswa { 
     public $nama;//atribut nama
     public $nim; //atribut nim
     public $jurusan; //atribut jurusan
 
-    public function __construct ($nama, $nim, $jurusan){
-        $this->nama = $nama;
-        $this->nim = $nim;
-        $this->jurusan = $jurusan;
-    }
-
+    
     public function tampilkanData() { //Menampilkan Data Mahasiswa
         return "Nama: $this->nama, NIM: $this->nim, Jurusan: $this->jurusan.";
     }
 
-    //Membuat Metode Tambahan
+    //Mengupdate Jurusan Mahasiswa
     public function updateJurusan($jurusanBaru) {
         $this->jurusan = $jurusanBaru;
     }
@@ -23,12 +18,19 @@ class Mahasiswa { //Membuat class mahasiswa
     // Setter untuk mengubah NIM
     public function setNim($nimBaru) {
         $this->nim = $nimBaru;
-}
+    }
 }
 //Instansiasi & Update Jurusan
-$mahasiswa1= new Mahasiswa("Devia Kippuw","230202031","Komputer dan Bisnis");
+$mahasiswa1= new Mahasiswa();
+$mahasiswa1->nama = "Devia Herena Kippuw";
+$mahasiswa1->nim = "230202031";
+$mahasiswa1->jurusan = "Komputer dan Bisnis";
+
+//Menampilkan Data Sebelum Update
 echo $mahasiswa1->tampilkanData()."<br>"; 
+
 //Update NIM
-$mahasiswa1->setNim ("230102032");
+$mahasiswa1->setNim("230102032");
+//Menampilkan Data Sesudah Update
 echo $mahasiswa1->tampilkanData();
 ?>
